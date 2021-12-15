9265ac46710df59c68fc52c16961e71003bc9a88
MayoIsASauce <60394656+MayoIsASauce@users.noreply.github.com>
2021-12-14 21:35:09 -0500

    Add files via upload

--- launcher.bat
+++ launcher.bat
@@ -1,6 +1,6 @@
 @echo off&setlocal
 for %%i in ("%~dp0.") do set "folder=%%~fi"
-cd /d %folder%
-curl https://raw.githubusercontent.com/Cars0419/LUG-INFO/main/ngrok_add.txt > %folder%\temp
+cd /d "%folder%"
+curl https://raw.githubusercontent.com/Cars0419/LUG-INFO/main/ngrok_add.txt > "%folder%\temp"
 title Lego Universe Gaming (US 1)
-%folder%\Patcher.py %folder%
+"%folder%\Patcher.py" "%folder%"
